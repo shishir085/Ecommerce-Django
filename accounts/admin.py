e344a21bd177a8f27b3051efc3737e84fb1ffe83
Shishir Pandey <shishirpandey087@gmail.com>
2025-11-02 20:43:30 +0545

    fix new errors and added base

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+
 # Register your models here.
+from .models import Profile
+
 admin.site.register(Profile)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
-from . import views
+from accounts.views import login_page,register_page , activate_email
 
 urlpatterns = [
-    path("login/",views.login_page,name="login_page"),
-    path("register/",views.register_page,name="signup_page")
+   path('login/' , login_page , name="login" ),
+   path('register/' , register_page , name="register"),
+   path('activate/<email_token>/' , activate_email , name="activate_email"),
 ]
